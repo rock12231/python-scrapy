6f6305bf670a5b0ce64fa78e898e1391e1729fe7
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-10-19 09:58:47 +0530

    Code Add Comment

--- movie/spiders/s_movie.py
+++ movie/spiders/s_movie.py
@@ -1,13 +1,14 @@
-from ast import Try
 import json
-from pkg_resources import yield_lines
 import scrapy
 
 class QuotesSpider(scrapy.Spider):
+    # Name of spider
     name = "main"
+    # URLs to crawl
     start_urls = ['https://www.justwatch.com/ag/movie/k-g-f-chapter-1']
 
     def parse(self, response):
+        # Get Data
         Genre = response.css('div.detail-infos__value span::text').getall()
         Director_Name = response.css('div.detail-infos__value span a::text').get()
         Cast = response.css('div.hidden-horizontal-scrollbar__items .title-credits__actor a::text').extract()
@@ -17,10 +18,14 @@ class QuotesSpider(scrapy.Spider):
         IMDb_Ratings = response.css('div.jw-scoring-listing__rating a::text').extract()
         Movie_name = response.css('div.title-block div h1::text').get()
         Movie_Link = response.request.url
+        
+        # Filter Data
         val = ", "
         Genre = [i for i in Genre if i != val]
         Release_year = Release_year.replace("(","")
         Release_year = Release_year.replace(")","")
+        
+        # Create Dictionary
         movie_json = {	
         "Genre" : Genre,
         "Director_Name" : Director_Name,
@@ -32,6 +37,7 @@ class QuotesSpider(scrapy.Spider):
         "Movie_name" : Movie_name,
         "Movie_Link" : Movie_Link
         }
+        
+        # Convert to data.JSON file
         with open('data.json', 'w') as f:     
             json.dump(movie_json, f)
-        # yield {'title' : title}
